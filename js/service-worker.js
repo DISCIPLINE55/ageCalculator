46860d406e3f626a592a6eecf14745ec4d4bbe33
DISCIPLINE55 <ismailmensah055@gmail.com>
2025-05-11 10:22:43 +0000

    v1 Updated

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,25 +1,45 @@
+const CACHE_NAME = 'age-calculator-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/script.js',
+  '/js/date-utils.js',
+  '/js/chinese-zodiac.js',
+  '/js/i18n.js',
+  '/js/share.js',
+  '/js/service-worker.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// Install: Cache core assets
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('age-calculator-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        'index.html',
-        'css/style.css',
-        'js/script.js',
-        'js/date-utils.js',
-        'js/chinese-zodiac.js',
-        'js/i18n.js',
-        'js/service-worker.js',
-        'js/share.js',
-        'icons/icon-192.png',
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
+  self.skipWaiting(); // Activate immediately after install
 });
 
+// Activate: Clean old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim(); // Take control of clients immediately
+});
+
+// Fetch: Serve cached assets or fetch from network
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
